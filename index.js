a4a549bf5d12050fb9943d7eedf338f01006d854
Lars <lars@recap.dk>
2025-03-18 21:05:39 +0100

    Final commit
    Added all the assignments

--- index.js
+++ index.js
@@ -1,21 +1,60 @@
 
 
 
-// variables - this doesnt seem to work in my console, i get "Jag heter ${firstName}". And i dont get the pretty colors in the code when i add the ''
+//first task
+
+console.log("Hello World!")
+
 let age = 48;
-let firstName = "Lars"
+let firstName = "Lars";
 let color = "warm_yellow";
-
-console.log('Jag heter ${firstName} och är ${age} somrar gammal, det är nog därför att jag gillar färgen ${color}');
-
 console.log(typeof age);
 console.log(typeof firstName);
 
-let x;
-x = 824;
+console.log(`Jag heter ${firstName} och är ${age} somrar gammal, det är nog därför att jag gillar färgen ${color}`);
 
-let y;
-y = 600;
+// second task
 
-console.log(x);
-console.log(y);
+
+let sith = 2;
+sith = sith -1;
+console.log (sith);
+
+let julen = 30;
+julen += 1;
+console.log(julen);
+
+let holes = 258;
+console.log(holes * holes);
+
+let birds = 1367;
+birds /=30;
+console.log(birds);
+
+// third task
+
+let apples = 8;
+let bananas = 4;
+
+console.log(apples-bananas);
+console.log(apples+bananas);
+console.log(apples*bananas);
+console.log(apples/bananas);
+console.log(apples/bananas+bananas);
+
+
+
+//---------------------------------------
+
+
+// let x;
+// x = 824;
+
+// let y;
+// y = 600;
+
+// console.log(x);
+// console.log(y);
+
+// let online = false;
+// console.log(`is this person online? ${online}`)
