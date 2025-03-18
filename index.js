dd953fb26c4496f3c663b443f3b8d4000790a481
Lars <lars@recap.dk>
2025-03-18 18:21:05 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+
+// variables - this doesnt seem to work in my console, i get "Jag heter ${firstName}". And i dont get the pretty colors in the code when i add the ''
+let age = 48;
+let firstName = "Lars"
+let color = "warm_yellow";
+
+console.log('Jag heter ${firstName} och är ${age} somrar gammal, det är nog därför att jag gillar färgen ${color}');
+
+console.log(typeof age);
+console.log(typeof firstName);
+
+let x;
+x = 824;
+
+let y;
+y = 600;
+
+console.log(x);
+console.log(y);
